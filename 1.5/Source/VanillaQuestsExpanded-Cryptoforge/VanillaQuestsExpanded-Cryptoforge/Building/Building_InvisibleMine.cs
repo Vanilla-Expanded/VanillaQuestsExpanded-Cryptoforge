f764bd38f1d92b74f65b1f7e40068385a4eb823a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-02-25 10:20:47 +0100

    Hidden mines

---
+++ 1.5/Source/VanillaQuestsExpanded-Cryptoforge/VanillaQuestsExpanded-Cryptoforge/Building/Building_InvisibleMine.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace VanillaQuestsExpandedCryptoforge
+{
+    public class Building_InvisibleMine : Building_Trap
+    {
+        public bool trapRevealed = false;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref this.trapRevealed, "trapRevealed");
+            
+        }
+        protected override void SpringSub(Pawn p)
+        {
+            GetComp<CompExplosive>().StartWick(p);
+            trapRevealed = true;
+        }
+        protected override void DrawAt(Vector3 drawLoc, bool flip = false)
+        {
+            base.DrawAt(drawLoc, flip);
+
+            if (trapRevealed)
+            {
+                var vector = DrawPos + Altitudes.AltIncVect;
+
+                vector.y += Altitudes.AltInc;
+                Vector3 vectorOffset = Vector3.zero;
+
+                GraphicsCache.VisibleMine.DrawFromDef(vector + vectorOffset, Rot4.North, null);
+
+            }
+        }
+
+    }
+}
