1ce2238555b5a2728200f84566bcc77be56e9c30
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-02-25 09:37:21 +0100

    Turret shields

---
+++ 1.5/Source/VanillaQuestsExpanded-Cryptoforge/VanillaQuestsExpanded-Cryptoforge/Building/Building_TurretGun_Armoured.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using Verse.Sound;
+
+
+
+namespace VanillaQuestsExpandedCryptoforge
+{
+    public class Building_TurretGun_Armoured : Building_TurretGun
+    {
+
+
+        public override void PreApplyDamage(ref DamageInfo dinfo, out bool absorbed)
+        {
+            if (Rand.Chance(0.35f))
+            {
+                Effecter effecter = EffecterDefOf.Deflect_Metal_Bullet.Spawn();
+                effecter.Trigger(this, this);
+
+                absorbed = true;
+            }
+            else { absorbed = false; }
+            
+
+        }
+    }
+}
