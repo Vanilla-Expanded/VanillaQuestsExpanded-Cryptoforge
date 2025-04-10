48d2d537873d8d4a89046ba1179e4ee53c29c14c
Taranchuk <nfhfyxer@gmail.com>
2025-04-10 12:54:07 +0300

    cryptoaxe slowdown formula changed

--- 1.5/Source/VanillaQuestsExpanded-Cryptoforge/VanillaQuestsExpanded-Cryptoforge/Verbs/Verb_MeleeAttackDamageCryptoaxe.cs
+++ 1.5/Source/VanillaQuestsExpanded-Cryptoforge/VanillaQuestsExpanded-Cryptoforge/Verbs/Verb_MeleeAttackDamageCryptoaxe.cs
@@ -11,7 +11,7 @@ namespace VanillaQuestsExpandedCryptoforge
             if (target.Thing is Pawn targetPawn)
             {
                 float heatArmor = targetPawn.GetStatValue(StatDefOf.ArmorRating_Heat);
-                float severity = (2f - heatArmor) / 4f;
+                float severity = (2f - heatArmor) / 10f;
                 if (severity < 0.01f) severity = 0.01f;
                 HealthUtility.AdjustSeverity(targetPawn, InternalDefOf.VQE_CryptoSlowdown, severity);
             }
