b805eec4a9b2fc48adda82181aa2aa39b1852474
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-06-23 15:21:18 +0200

    v2.02

---
+++ 1.6/Source/VanillaQuestsExpanded-Cryptoforge/VanillaQuestsExpanded-Cryptoforge/Building/StudiableBuilding_Cryptoforge.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using Verse.Sound;
+using static HarmonyLib.Code;
+using Verse.Noise;
+using Verse.AI;
+
+using VEF.Buildings;
+
+
+namespace VanillaQuestsExpandedCryptoforge
+{
+    public class StudiableBuilding_Cryptoforge : StudiableBuilding
+    {
+        public override void Study(Pawn pawn)
+        {
+            var site = Map.Parent;
+            var signal = "Studied_" + this.def.defName;
+            Find.SignalManager.SendSignal(new Signal(signal, site.Named("SUBJECT")));
+            QuestUtility.SendQuestTargetSignals(site.questTags, signal, site.Named("SUBJECT"));
+            base.Study(pawn);
+            
+        }
+    }
+}
